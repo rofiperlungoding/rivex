5b9ebbc5952ce4b0629968912811b9472038b126
rofiperlungoding <opikopi32@gmail.com>
2025-06-07 16:02:22 +0700

    Remove Gallery and Projects references

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,7 @@ import { ThemeProvider } from './contexts/ThemeContext';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
-import Projects from './pages/Projects';
 import News from './pages/News';
-import Gallery from './pages/Gallery';
 import Timeline from './pages/Timeline';
 
 function App() {
@@ -17,9 +15,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
             <Route path="/news" element={<News />} />
-            <Route path="/gallery" element={<Gallery />} />
             <Route path="/timeline" element={<Timeline />} />
           </Routes>
         </Layout>
